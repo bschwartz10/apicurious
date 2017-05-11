82e42dd4a6c852ca4c4c79c661eabb611d033249
bschwartz10 <bschwartz92@gmail.com>
2017-05-11 14:18:36 -0600

    add username model column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509234709) do
+ActiveRecord::Schema.define(version: 20170511173806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170509234709) do
     t.string "uid"
     t.string "name"
     t.string "oauth_token"
+    t.string "username"
   end
 
 end
