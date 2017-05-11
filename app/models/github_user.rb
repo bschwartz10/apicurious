bd40ff4a13e66e3172cb37fec5e6d5cd317293c1
bschwartz10 <bschwartz92@gmail.com>
2017-05-10 18:20:32 -0600

    add starred/followers method to githubuser model
    starred method returns an array of starred repos
    followers method return an array of the current users followers

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,15 +1,26 @@
 class GithubUser
-attr_reader :avatar_url, :username
+attr_reader :avatar_url, :username, :followers
 
 def initialize(attrs = {})
   @attrs = attrs
   @avatar_url = attrs[:avatar_url]
   @username = attrs[:login]
+  @followers = attrs[:followers_url]
 end
 
   def self.find(token)
     user = GithubService.users_by(token)
     new(user)
+  end
+
+  def self.starred(token)
+    GithubService.starred_by(token)
+  end
+
+  def self.followers(token)
+    GithubService.followers_by(token).map do |user|
+      Follower.new(user)
+    end
   end
 
   private
