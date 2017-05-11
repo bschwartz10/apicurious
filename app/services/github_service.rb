1d7f2727c6a871b7b3219781019659c0899e80a8
bschwartz10 <bschwartz92@gmail.com>
2017-05-10 18:21:44 -0600

    create starred_by and followers_by methods
    starred_by method hits the starred endpoint and returns and array of
    repos
    followers_by method hits the followers endpoint and returns an array of
    followers

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -15,6 +15,27 @@ class GithubService
     service.users_by(token)
   end
 
+  def starred_by(token)
+    response = conn.get("/user/starred", {access_token: token})
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.starred_by(token)
+    service = GithubService.new
+    service.starred_by(token)
+  end
+
+  def self.followers_by(token)
+    service = GithubService.new
+    service.followers_by(token)
+  end
+
+  def followers_by(token)
+    response = conn.get("/user/followers", {access_token: token})
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   private
     def conn
       @_conn
