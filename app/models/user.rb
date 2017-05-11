8e54582f7f4c593715b6571b1c44bd43ddb0ae47
bschwartz10 <bschwartz92@gmail.com>
2017-05-11 14:16:59 -0600

    append nickname to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ def self.from_omniauth(auth_info)
   where(uid: auth_info[:uid]).first_or_create do |new_user|
     new_user.uid = auth_info.uid
     new_user.name = auth_info.info.name
+    new_user.username = auth_info.info.nickname
     new_user.oauth_token = auth_info.credentials.token
   end
 end
